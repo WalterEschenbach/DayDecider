beceab4a31a0f582c91a7adf4cad5c167498b8ee
Walter <eschenbach.walter@gmail.com>
2021-04-15 11:10:02 -0400

    refactor EventContainer

--- ddclient/src/components/detailContainer/DetailContainer.js
+++ ddclient/src/components/detailContainer/DetailContainer.js
@@ -1,51 +1,24 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import EventContainer from '../eventContainer/EventContainer'
 import GroupContainer from '../groupContainer/GroupContainer'
 import {Accordion, Card} from 'react-bootstrap'
-import EventItem from '../eventItem/EventItem'
 
 import './detailcontainer.css'
 
 
 export default function DetailContainer(props) {
-    const [data, setData] = useState(props.data)
-    const [eventFocus, setEventFocus] = useState(props.eventFocus)
     const [activeKey, setActiveKey] = useState("0")
-    
-    useEffect(()=>{
-        setData(props.data)
-        props.setEventFocus(eventFocus)
-    }, [props.data, props.eventFocus, props, eventFocus])
-
-    const onClick = (index) => {
-        setEventFocus(index.target.innerText);
-        setActiveKey("1")
-    }
 
     return (
         <div className="dContainer">
              <Accordion defaultActiveKey="0" activeKey={activeKey}>
-                    <Card >
-                        <Accordion.Toggle 
-                        as={Card.Header} 
-                        eventKey="0" 
-                        className="aToggle"
-                        onClick={()=>setActiveKey("0")}
-                        style={{backgroundColor: "#17252A", color: "#FEFFFF"}}
-                        >
-                            <EventContainer/>
-                        </Accordion.Toggle>
-                        {data.map((event)=>{
-                        const className = eventFocus === event.eventName ? "media-active" : 'media';
-                        return(
-                                <EventItem
-                                onClick={onClick}
-                                eventFocus={eventFocus}
-                                className={className} 
-                                event={event} 
-                                key={event.name}
-                                />
-                            )})}
+                    <Card>
+                        <EventContainer 
+                        data={props.data} 
+                        eventFocus={props.eventFocus} 
+                        setEventFocus={props.setEventFocus}
+                        setActiveKey={setActiveKey} 
+                        activeKey={activeKey}/>
                     </Card>
                     <Card>
                         <GroupContainer group={props.group}/>
--- ddclient/src/components/eventContainer/EventContainer.js
+++ ddclient/src/components/eventContainer/EventContainer.js
@@ -2,24 +2,48 @@ import React, {useState} from 'react'
 import {Button} from 'react-bootstrap'
 import CreateEvent from '../createEvent/CreateEvent'
 import {FaRegCalendarPlus} from 'react-icons/fa'
+import {Accordion, Card} from 'react-bootstrap'
+import EventItem from '../eventItem/EventItem'
 import './eventcontainer.css'
 
-// const url = `${Settings.domain.server}/event/find`;
 
-export default function EventContainer() {
+export default function EventContainer(props) {
     const [modalShow, setModalShow] = useState(false)
 
+    const onClick = (index) => {
+        props.setEventFocus(index.target.innerText);
+        props.setActiveKey("1")
+    }
+    
     return (
         <React.Fragment>
-            <div className="eContainer">
-                <h2>EVENTS</h2>
-                <Button className="create-event-btn" variant="secondary" onClick={()=>setModalShow(true)}><FaRegCalendarPlus/></Button>
-            </div>
-            <CreateEvent
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-            />
+            <Accordion.Toggle 
+            as={Card.Header} 
+            eventKey="0" 
+            className="aToggle"
+            onClick={()=>props.setActiveKey("0")}
+            style={{backgroundColor: "#17252A", color: "#FEFFFF"}}
+            >
+                <div className="eContainer">
+                    <h2>EVENTS</h2>
+                    <Button className="create-event-btn" variant="secondary" onClick={()=>setModalShow(true)}><FaRegCalendarPlus/></Button>
+                </div>
+                <CreateEvent
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+                />
+            </Accordion.Toggle>
+            {props.data.map((event)=>{
+                const className = props.eventFocus === event.eventName ? "media-active" : 'media';
+                return(
+                    <EventItem
+                    onClick={onClick}
+                    eventFocus={props.eventFocus}
+                    className={className} 
+                    event={event} 
+                    key={event.name}
+                    />
+                    )})}
         </React.Fragment>
-     
     )
 }
