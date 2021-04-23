69b34f974a07c5905aaea1de85bffd812f89eb58
Walter <eschenbach.walter@gmail.com>
2021-04-22 20:57:09 -0500

    updated formatting for account-btn

--- ddclient/src/components/auth/account/Account.js
+++ ddclient/src/components/auth/account/Account.js
@@ -1,38 +1,38 @@
-import React, {useEffect, useState} from 'react'
-import {useLocation} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 import queryString from 'query-string'
-import {Button} from 'react-bootstrap'
-import {FaAngleDown} from 'react-icons/fa'
+import { Button } from 'react-bootstrap'
+import { FaAngleDown } from 'react-icons/fa'
 import AccountModal from '../accountModal/AccountModal'
 import './account.css'
 
 
 export default function Account(props) {
-    const {search} = useLocation();
-    const {email, picture} = queryString.parse(search)
+    const { search } = useLocation();
+    const { email, picture } = queryString.parse(search)
     const [name, setName] = useState(email);
     const [pic, setPic] = useState(picture);
     const [modalShow, setModalShow] = useState(false);
 
-    useEffect(()=>{
+    useEffect(() => {
         setName(email)
         setPic(picture)
-    },[email, picture]);
+    }, [email, picture]);
 
 
     return (
         <React.Fragment>
-                <Button className="account-btn" variant="outline-primary" onClick={()=>setModalShow(true)}>
-                    <FaAngleDown className="aIcon" />
-                    <div className="name"><h3>{name}</h3></div>
-                    <img alt="MJ_Profile" src={pic} className="profile-pic">{props.pic}</img>
-                </Button> 
-                       
-                <AccountModal
-                    show={modalShow}
-                    onHide={() => setModalShow(false)}
-                />
+            <button className="account-btn" variant="outline-primary" onClick={() => setModalShow(true)}>
+                <FaAngleDown className="aIcon" />
+                <div className="name"><h3>{name}</h3></div>
+                <img alt="MJ_Profile" src={pic} className="profile-pic">{props.pic}</img>
+            </button>
+
+            <AccountModal
+                show={modalShow}
+                onHide={() => setModalShow(false)}
+            />
         </React.Fragment>
-     
+
     )
 }
