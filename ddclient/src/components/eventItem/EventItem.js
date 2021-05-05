547df4131e6995d8f601e4ad727c31c3613043ee
Walter <eschenbach.walter@gmail.com>
2021-05-05 16:30:55 -0400

    minor styling

--- ddclient/src/components/eventItem/EventItem.js
+++ ddclient/src/components/eventItem/EventItem.js
@@ -6,7 +6,7 @@ export default function EventItem(props) {
 
     return (
             <Accordion.Collapse onClick={props.onClick} key={props.event} className={props.className} eventKey="0" >
-                <Card.Body><h4>{props.event.eventName}</h4></Card.Body>
+                <Card.Body><h4 style={{textShadow: "2px 2px 4px #b9b9b9;"}}>{props.event.eventName}</h4></Card.Body>
             </Accordion.Collapse>
         
    
