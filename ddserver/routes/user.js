b7c0f69212cc15ec2ba12f89ec3274510c2e12b4
Walter <eschenbach.walter@gmail.com>
2021-05-19 10:42:49 -0400

    comment for user route and removed /events/find route

--- ddserver/routes/user.js
+++ ddserver/routes/user.js
@@ -2,11 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.model')
 
 
-
-router.get('/events/find', (req,res)=>{
-    res.send(req.user.events).end()
-})
-
+// Get User Info for UserProvider
 router.get('/find', (req, res)=>{
     if(req.user){
         res.send(req.user).end()
