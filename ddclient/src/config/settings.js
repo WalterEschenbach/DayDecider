309f54d91ca8e97ca2b29848f3f60b0d709a12da
Walter <eschenbach.walter@gmail.com>
2021-05-19 13:21:01 -0400

    minor syntax

--- ddclient/src/config/settings.js
+++ ddclient/src/config/settings.js
@@ -1,6 +1,6 @@
 const Settings = {
     domain: {
-        client: "https://fierce-spire-73209.herokuapp.com/",
+        client: "https://fierce-spire-73209.herokuapp.com",
         server: "http://127.0.0.1:3030"
     }
     
